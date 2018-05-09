554e9ca086976224f2d27148c325b787525d2069
khoa huynh <yumiling1001@gmail.com>
2018-05-09 11:34:01 +0700

    make it better

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 // Initialize pool
 var pool      =    mysql.createPool({
     connectionLimit : 100,
-    host     : 'localhost',
-    user     : 'root',
-    password : 'lovekieutrinh97',
-    database : 'showbiz',
+    host     :  'db4free.net'.
+    user     : 'huynhkhoa',
+    password : '123123123',
+    database : 'showbiz266',
     debug    :  false
 });
 module.exports = pool;
